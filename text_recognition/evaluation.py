104731a5e9c02711148d30ce075bd3336c819257
aleksandr-fedotov-jb <aleksandr.fedotov@jetbrains.com>
2023-10-24 21:36:12 +0300

    Verbose option for more prints of evaluate function

--- text_recognition/evaluation.py
+++ text_recognition/evaluation.py
@@ -10,7 +10,16 @@ def text_accuracy(src, recognized):
     return 1 - dist / max(len(src), len(recognized))
 
 
-def evaluate_model(model: OCRModel, dataset_path: Path, ):
+def evaluate_model(model: OCRModel, dataset_path: Path, verbose=0):
+    """
+    Evaluate a given OCR model on a dataset.
+
+    :param model: The OCRModel object to evaluate.
+    :param dataset_path: The path to the dataset directory.
+    :param verbose: Level of verbosity. 0 to print nothing, 1 to print only accuracy for each file,
+     2 to print both accuracy and predicted/ground truth text. Default is 0.
+    :return: The mean accuracy of the model.
+    """
     with open(dataset_path / 'info.json') as f:
         dataset_info = json.load(f)
     accuracies = []
@@ -18,4 +27,11 @@ def evaluate_model(model: OCRModel, dataset_path: Path, ):
         predicted_text = model.recognize_text(dataset_path / file_info['path'])
         accuracy = text_accuracy(file_info['text'], predicted_text)
         accuracies.append(accuracy)
+        if verbose >= 1:
+            print(file_info['path'], accuracy)
+        if verbose >= 2:
+            print(f'Predicted:\n\n'
+                  f'"{predicted_text}"\n'
+                  f'Ground truth:\n\n'
+                  f'"{file_info["text"]}"\n')
     return np.mean(accuracies)
